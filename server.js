bc9736dac682ca7951b839d2c07bb562c102cb90
Dishanth Surya Tej <dishanth242006@gmail.com>
2026-06-26 16:29:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false })); 
 
 const PORT = process.env.PORT || 3000;
 
